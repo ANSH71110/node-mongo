a978caf3e78d287f4b391e3418bcf68b655813eb
ANSH71110 <anshbale11@gmail.com>
2022-03-07 16:54:10 +0530

    Nde Mongo Example Part 2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const MongoClient=require('mongodb').MongoClient;
 const assert=require('assert');
+const dbupper=require('./operations');
 
 const url='mongodb://localhost:27017';
 const dbname='conFusion';
@@ -8,7 +9,29 @@ MongoClient.connect(url,(err,client)=>{
     assert.equal(err,null);
 
     const db =client.db(dbname);
-    const collection=db.collection('dishes');
+
+    dbupper.insertDocument(db,{name:'ansh',description:'text'},'dishes',(result)=>{
+        console.log("Insert Document:\n",result.ops);
+
+        dbupper.findDocument(db,'dishes',(docs)=>{
+            console.log('Found Documents:\n',docs);
+
+            dbupper.updateDocument(db,{name:'ansh'},{description:'updates text'},'dishes',(result)=>{
+                console.log('Updated Documents:\n',result.result);
+
+                dbupper.findDocument(db,'dishes',(docs)=>{
+                    console.log('Found Documents:\n',docs);
+
+                    db.dropCollection('dishes',(result)=>{
+                        console.log('Dropped Collection: ',result);
+                        client.close();
+                    });
+                });
+            });
+        });
+    });
+
+    /*const collection=db.collection('dishes');
 
     collection.insertOne({"name":"ballui","description":"test"},(err,result)=>{
         assert.equal(err,null);
@@ -28,5 +51,5 @@ MongoClient.connect(url,(err,client)=>{
                 client.close();
             });
         });
-    });
+    });*/
 });
